16b6a9ffa6191be2f95da16245ea770e11de4018
KevinMartinez45 <martinezperezkevin@cticoban.edu.gt>
2023-06-26 22:22:17 -0600

    Avance 26-06-2023

---
+++ estructura de datos basicas/Elimina elementos usando splice().js
@@ -0,0 +1,6 @@
+const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+// Cambia solo el código debajo de esta línea
+arr.splice(4,4);
+arr.shift();
+// Cambia solo el código encima de esta línea
+console.log(arr);
